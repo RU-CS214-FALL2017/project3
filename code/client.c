800c7afc035fab9876b0f3d60b00d6d1ab315ff2
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2017-12-03 12:40:11 -0500

    made connect to socket

--- code/client.c
+++ code/client.c
@@ -7,37 +7,17 @@
 #include <unistd.h>
 
 #include "mainTools.h"
+#include "tools.h"
 
 int main(int argc, char ** argv) {
     
     char * hostname = getHostname(argc, argv);
     char * port = getPort(argc, argv);
     
-    struct addrinfo hints;
-    bzero(&hints, sizeof(struct addrinfo));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_V4MAPPED;
-    
-    struct addrinfo * addr;
-    
-    if (getaddrinfo(hostname, port, &hints, &addr) != 0) {
-        
-        fprintf(stderr, "Error finding ip address");
-        exit(EXIT_FAILURE);
-    };
-    
     int sockFd = socket(AF_INET, SOCK_STREAM, 0);
-    if (connect(sockFd, addr->ai_addr, sizeof(struct sockaddr)) == -1) {
-        
-        fprintf(stderr, "Error connecting");
-        exit(EXIT_FAILURE);
-    }
     
-    char send[100];
+    connectToSocket(sockFd, hostname, port);
     
-    bzero(send, 100);
-    fgets(send, 100, stdin);
+    char * send = "hellp\nworld";
     write(sockFd, send, 100);
-    
 }
---
+++ code/tools.h
@@ -0,0 +1,6 @@
+#ifndef tools_h
+#define tools_h
+
+void connectToSocket(int socket, const char * hostname, const char * port);
+
+#endif /* tools_h */
