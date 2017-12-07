a7d02d4282a5330f0707474e151bf28e8923302c
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2017-12-07 11:23:32 -0500

    added stuff

--- code/csvStore.c
+++ code/csvStore.c
@@ -3,16 +3,17 @@
 #include "syncWraps.h"
 
 #include "tools.h"
+#include "sorter.h"
 
 pthread_mutex_t ListMutex = PTHREAD_MUTEX_INITIALIZER;
 
 struct CsvsNode {
     
-    pthread_mutex_t lock;
+//    pthread_mutex_t lock;
     uint32_t id;
     unsigned int sortIndex;
     int isNumericColumn;
-    struct Table table;
+    struct Table * table;
     struct CsvsNode * next;
     
 } * Head = NULL;
@@ -23,17 +24,71 @@ void initializeId(uint32_t id, unsigned int sortIndex) {
     mutexInit(&(newNode->lock), "CsvsNodeLock");
     newNode->id = id;
     newNode->sortIndex = sortIndex;
-    newNode->table.rows = 0;
-    newNode->table.columns = 0;
+    newNode->table = NULL;
     newNode->next = NULL;
     
     mutexLock(&ListMutex, "CsvsListLock");
     
     newNode->next = Head;
     Head = newNode;
+    
+    mutexUnlock(&ListMutex, "CsvsListLock");
 }
 
-void addTable(struct Table * table, uint32_t id) {
+int addTable(struct Table * table, uint32_t id) {
     
+    mutexLock(&ListMutex, "CsvsListLock");
     
+    for (struct CsvsNode * i = Head; i != NULL; i = Head->next) {
+        
+        if (i->id == id) {
+            
+//            mutexLock(&(i->lock), "TableLock");
+            
+            if (i->table == NULL) {
+                i->table = table;
+                
+            } else if (sameHeaders(i->table, table)) {
+                i->table = mergeTables(i->table, table, i->sortIndex, i->isNumericColumn);
+                
+            } else {
+                
+                fprintf(stderr, "Table not compatible.\n");
+//                mutexUnlock(&(i->lock), "TableLock");
+                return 0;
+            }
+            
+//            mutexUnlock(&(i->lock), "TableLock");
+            return 1;
+        }
+    }
+    
+    mutexUnlock(&ListMutex, "CsvsListLock");
+    
+    fprintf(stderr, "ID %u not found\n", id);
+    return 0;
+}
+
+struct Table * dumpTable(uint32_t id) {
+    
+    mutexLock(&ListMutex, "CsvsListLock");
+    
+    struct CsvsNode * temp = Head;
+    
+    for (struct CsvsNode * i = Head; i != NULL; i = Head->next) {
+        
+        if (i->id == id) {
+            
+            temp->next = i->next;
+            struct Table * ret = i->table;
+            free(i);
+            return ret;
+        }
+        
+        temp = i;
+    }
+    
+    mutexUnlock(&ListMutex, "CsvsListLock");
+    
+    return NULL;
 }
